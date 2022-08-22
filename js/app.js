86cff11289b14027cf8fadfba1750641b5dcda98
HamimHossain <hamim.mgp@gmail.com>
2022-08-23 00:25:54 +0600

    final push

--- js/app.js
+++ js/app.js
@@ -1,18 +1,15 @@
 const playersList = [];
 
 // set player name list on display //
-
 function showPlayersName(playersList){
     const nameList = document.getElementById('name-list');
     nameList.innerText = '';
     console.log(nameList)
 
     for(let i = 0; i < playersList.length; i++){
-
         const playersNameList = playersList[i];
         const tr = document.createElement('tr');
         tr.innerHTML = `
-
         <th class="text-slate-400">${i+1}.</th>
         <th class="text-slate-400">${playersNameList}</th>
         `;
@@ -21,7 +18,6 @@ function showPlayersName(playersList){
 };
 
 // get players name list //
-
 function addToList(element){
     const playersName = element.parentNode.parentNode.children[0].innerText;
     if(playersList.length<5){
@@ -38,6 +34,10 @@ function addToList(element){
 document.getElementById('btn-calculate').addEventListener('click', function(){
     const totalPlayers = playersList.length;
     const perPlayerValue = getInputValue('per-player-input-value')
+    if(isNaN(perPlayerValue)){
+        alert('Please give number.')
+        return;
+    }
     const totalPlayersExpenses = totalPlayers * perPlayerValue;
 
     const playerExpenses = getElementValue('player-expenses');
@@ -46,14 +46,15 @@ document.getElementById('btn-calculate').addEventListener('click', function(){
 
 
 // calculate total expenses//
-
 document.getElementById('btn-calculate-total').addEventListener('click', function(){
     const managerCost = getInputValue('manager-cost');
     
     const coachCost = getInputValue('coach-cost');
-    
+    if(isNaN(managerCost) || isNaN(coachCost)){
+        alert('Please enter number in both input fields.')
+        return;
+    }
     const playerExpenses = getElementValue('player-expenses');
-    
     const totalAmount = playerExpenses+managerCost+coachCost;
     const total = getElementValue('total');
     setValue('total',totalAmount );
