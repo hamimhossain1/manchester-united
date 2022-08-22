2cb31907f02d75f34f1e37914c779a8170e4312a
HamimHossain <hamim.mgp@gmail.com>
2022-08-22 23:22:21 +0600

    js nere to finish

--- js/app.js
+++ js/app.js
@@ -1,3 +1,50 @@
+const playersList = [];
+
+function showPlayersName(playersList){
+    const nameList = document.getElementById('name-list');
+    nameList.innerText = '';
+    console.log(nameList)
+
+    for(let i = 0; i < playersList.length; i++){
+
+        const playersNameList = playersList[i];
+        const tr = document.createElement('tr');
+        tr.innerHTML = `
+
+        <th class="text-slate-400">${i+1}.</th>
+        <th class="text-slate-400">${playersNameList}</th>
+        `;
+        nameList.appendChild(tr);
+
+        console.log(playersNameList)
+    }
+
+    
+};
+
+
+function addToList(element){
+    const playersName = element.parentNode.parentNode.children[0].innerText;
+    
+    playersList.push(playersName);
+
+    showPlayersName(playersList);
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 document.getElementById('btn-calculate').addEventListener('click', function(){
     const perPlayerValue = getInputValue('per-player-input-value')
     // console.log(perPlayerValue)
